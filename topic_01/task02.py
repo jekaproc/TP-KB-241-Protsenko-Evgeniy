f29f83e6468ce463a43ddd4538f08ea1d35cc74e
SPARDA <protsenkoYa1005@gmail.com>
2025-09-13 19:10:07 +0300

    add solution for topic 01~

--- topic_01/task02.py
+++ topic_01/task02.py
@@ -1,2 +1,7 @@
-print ("Hello World!")
-print ("Second Hello!")
+text = " hellO worlD "
+
+print("strip():", text.strip())
+print("capitalize():", text.capitalize())
+print("title():", text.title())
+print("upper():", text.upper())
+print("lower():", text.lower())
